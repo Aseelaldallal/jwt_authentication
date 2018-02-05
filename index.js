45afc25b972ad6362224495694c6ea3c7ef66c62
unknown <aseelaldallal@gmail.com>
2018-02-05 14:15:00 -0500

    add passport-jwt strategy

--- index.js
+++ index.js
@@ -10,8 +10,44 @@ const jwt = require('jsonwebtoken');
 
 const passport = require('passport');
 const passportJWT = require("passport-jwt");
-const ExtractJwt = passportJWT.ExtractJwt;
-const JwtStrategy = passportJWT.strategy;
+const JwtStrategy = require('passport-jwt').Strategy;
+const ExtractJwt = require('passport-jwt').ExtractJwt;
+
+// This normally would come from the db. Faking it here.
+let users = [
+    {
+        id: 1,
+        name: 'Aseel',
+        password: '!hi!'
+    },
+    {
+        id: 2,
+        name: 'test',
+        password: 'blahtest'
+
+    }
+]
+
+
+// Passport strategies are middleware functions that requests run through before getting to the actual route.
+// If authentication strategy fails, route will not be called and a 401 unauthorized response will be sent.
+let jwtOptions = {};
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+jwtOptions.secretOrKey = 'bloooper';
+
+const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
+    console.log('payload recieved: ', jwt_payload);
+    let user = users[_.findIndex(users, { id: jwt_payload.id })];
+    if (user) {
+        next(null, user);
+    } else {
+        next(null, false);
+    }
+});
+
+passport.use(strategy);
+
+
 
 
 app.get("/", (req, res) => {
