f9db1c7c4b261c550705bb26fcffbf26284bfccb
unknown <aseelaldallal@gmail.com>
2018-02-05 14:22:48 -0500

    add login route

--- index.js
+++ index.js
@@ -32,7 +32,8 @@ let users = [
 // Passport strategies are middleware functions that requests run through before getting to the actual route.
 // If authentication strategy fails, route will not be called and a 401 unauthorized response will be sent.
 let jwtOptions = {};
-jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken();
+// fromAuthHeaderAsBearerToken() creates a new extractor that looks for the JWT in the authorization header with the scheme 'bearer'
+jwtOptions.jwtFromRequest = ExtractJwt.fromAuthHeaderAsBearerToken(); // 
 jwtOptions.secretOrKey = 'bloooper';
 
 const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
@@ -48,10 +49,31 @@ const strategy = new JwtStrategy(jwtOptions, (jwt_payload, next) => {
 passport.use(strategy);
 
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
     res.json({ message: "Express is up!" });
+});
+
+
+app.post('/login', (req, res) => {
+    if (req.body.name && req.body.password) {
+        const name = req.body.name;
+        const password = req.body.password;
+    }
+    const user = users[_.findIndex(users, { name: name })]; // usually a db call
+    if (!user) {
+        res.status(401).json({ message: "no such user found" });
+    }
+    if (user.password === req.body.password) {
+        // from now on we'll dientify the user by the id and the id is the only personalized value that goes into our token
+        var payload = { id: user.id };
+        var token = jwt.sign(payload, jwtOptions.secretOrKey); // using jwtwebpackage
+        res.json({ message: "ok", token: token });
+    } else {
+        res.status(401).json({ message: "passwords did not match" });
+    }
 });
 
 app.listen(3000, () => {
